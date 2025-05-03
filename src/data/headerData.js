320333c06fa1b8ac2ce9251af71c37fe1ae8037e
Conor Fabian <conorfabian3@gmail.com>
2025-05-02 22:35:58 -0700

    setup + minor changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
-    title: "Web Developer",
-    desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    name: 'Conor Fabian',
+    title: "Software development, Full Stack Development, ML/AI",
+    desciption:"Hi, I'm Conor, currently an undergraduate student at UC Riverside studying Computer Science. I work as a Software Engineering Intern at the UCR School of Business, where I continue to grow my technical and collaborative skills. I'm deeply interested in exploring machine learning--particularly large language models, natural language processing and computer vision.",
+    image: 'https://avatars.githubusercontent.com/u/110637682?s=400&u=8f32a423ddfb8e91c3b4ae4d917378d2b3629ec9&v=4',
     resumePdf: resume
 }
--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,7 +1,7 @@
 export const socialsData = {
-    github: 'https://github.com/',
+    github: 'https://github.com/conorfabian',
     facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/in',
+    linkedIn: 'https://www.linkedin.com/in/conor-fabian/',
     instagram: 'https://www.instagram.com/',
     codepen: 'https://codepen.io/',
     twitter: 'https://twitter.com/',
