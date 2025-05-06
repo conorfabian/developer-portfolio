5d2983d5229101cb667a5ab46bab872448184000
Conor Fabian <conorfabian3@gmail.com>
2025-05-05 17:58:18 -0700

    udated profile picture

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/Conor_Fabian_Resume_Software_Engineering (1).pdf'
+import headerImage from '../assets/png/1662331027417.jpg'
 
 export const headerData = {
     name: 'Conor Fabian',
     title: "Software Development, Full Stack Development, ML/AI",
     desciption:"Hi, I'm Conor, currently an undergraduate student at UC Riverside studying Computer Science. I work as a Software Engineering Intern at the UCR School of Business, where I continue to grow my technical and collaborative skills. I'm deeply interested in exploring machine learning--particularly large language models, natural language processing and computer vision.",
-    image: 'https://avatars.githubusercontent.com/u/110637682?s=400&u=8f32a423ddfb8e91c3b4ae4d917378d2b3629ec9&v=4',
+    image: headerImage,
     resumePdf: resume
 }
